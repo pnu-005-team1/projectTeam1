cd1c58b8fd7f8952bd96b0dc98ed5e941277e7d0
Seunghoon Oh <stdsalmon@gmail.com>
2019-03-27 19:05:51 +0900

    Add google map api key and show PNU 컴퓨터공학관 #MBBC-7

--- android/app/src/main/java/kr/ac/pnu/cse/menumapproject/MapsActivity.java
+++ android/app/src/main/java/kr/ac/pnu/cse/menumapproject/MapsActivity.java
@@ -24,23 +24,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng pnucse = new LatLng(35.230996, 129.082302);
+        mMap.addMarker(new MarkerOptions().position(pnucse).title("컴퓨터공학관"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(pnucse, 11));
     }
 }
